b264d3d55ab0e7d72e4da92206538dfd971466dd
Belkins18 <nikolay.belibov@gmail.com>
2021-05-11 03:30:53 +0300

    CSS modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import cn from "classNames";
+import s from "./App.modules.scss";
+import "./custom.css"
 const App = () => {
     return (
-        <div>
+        <div className={cn(s.header, "color")}>
             This is Component!
         </div>
     )
