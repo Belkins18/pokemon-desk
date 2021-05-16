2d46ebb19cf6e142006d48e3e4dbb112ca5b4634
Belkins18 <nikolay.belibov@gmail.com>
2021-05-17 02:05:35 +0300

    ReactFC и Props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Layout from '../../components/Layout';
+import Button from '../../components/Button';
 
 import s from './Home.module.scss';
 
@@ -7,6 +9,15 @@ const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <h1>
+            <b>Find</b> all your favorite <b>Pokemon</b>
+          </h1>
+          <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          <Button onClick={() => console.log('Click')}>See pokemons</Button>
+        </div>
+      </Layout>
     </div>
   );
 };
